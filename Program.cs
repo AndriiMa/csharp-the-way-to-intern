f42b1258c6b9c24b60f5af99edeca63c0b38614a
Andrew Maslyuk <andriima@tahometer.com>
2020-08-06 03:31:14 +0300

    Added application starter

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using InternshipTest.Person;
+using InternshipTest.Institution;
+using InternshipTest.Institution.InterLink;
+
+namespace csharp_oop_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            University university = new University("CH.U.I.");
+            university.AddStudent(new Student("Andrew Kostenko", new Knowledge(40)));
+            university.AddStudent(new Student("Julia Veselkina", new Knowledge(60)));
+            university.AddStudent(new Student("Maria Perechrest", new Knowledge(60)));
+
+            Internship internship = new Internship("Interlink");
+            Console.WriteLine("List of internship's students:");
+            Console.WriteLine(internship.GetStudents());
+        }
+    }
+}
